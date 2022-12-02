240225df5030b32d9fee91d32545eec3035f6766
Igor Smolkov <smolkov.igor@gmail.com>
2022-12-02 20:27:56 +0530

    fix: Pretty warn

--- src/components/Disc/index.tsx
+++ src/components/Disc/index.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from 'react';
 
-import './disc.css'
+import './disc.css';
 
 type Props = {
   borderColor?: string;
-}
+};
 
 const Disc: FC<Props> = ({ borderColor = '' }) => {
-  return <div className="disc" style={{ borderColor }} />
+  return <div className="disc" style={{ borderColor }} />;
 };
 
 export { Disc, Props };
