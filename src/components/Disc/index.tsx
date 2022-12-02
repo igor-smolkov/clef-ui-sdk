1bde466d46142b720856d12371106baa0f5511f1
Igor Smolkov <smolkov.igor@gmail.com>
2022-12-02 20:05:48 +0530

    feat: Add component

---
+++ src/components/Disc/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+import './disc.css'
+
+type Props = {
+  borderColor?: string;
+}
+
+const Disc: FC<Props> = ({ borderColor = '' }) => {
+  return <div className="disc" style={{ borderColor }} />
+};
+
+export type { Props };
+
+export { Disc };
