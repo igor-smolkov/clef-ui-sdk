1217ad62c1635749c538d8c2f6557bb5cb3477f9
Igor Smolkov <smolkov.igor@gmail.com>
2022-12-02 22:54:42 +0530

    fix: Lint correct

--- src/components/Disc/index.tsx
+++ src/components/Disc/index.tsx
@@ -16,34 +16,36 @@ type Props = {
   onRendered?: () => void;
 };
 
-const Disc = forwardRef<HTMLDivElement, Props>(({ colors = undefined, isAnimate = false, borderColor = '', onRendered }, ref) => {
-  const [isRendering, setIsRendering] = useState(true);
+const Disc = forwardRef<HTMLDivElement, Props>(
+  ({ colors = undefined, isAnimate = false, borderColor = '', onRendered }, ref) => {
+    const [isRendering, setIsRendering] = useState(true);
 
-  const angle = colors ? (colors.reduce((acc, color) => acc + color) / colors.length) * 360 : 0;
+    const angle = colors ? (colors.reduce((acc, color) => acc + color) / colors.length) * 360 : 0;
 
-  const handleRendered = async () => {
-    setIsRendering(false);
-    onRendered?.();
-  };
+    const handleRendered = async () => {
+      setIsRendering(false);
+      onRendered?.();
+    };
 
-  const gradientCircleFallbackRender = useCallback(() => <DefaultCircle angle={angle} />, [angle]);
+    const gradientCircleFallbackRender = useCallback(() => <DefaultCircle angle={angle} />, [angle]);
 
-  return (
-    <div className={cn('clef-disc', { 'clef-disc_animate': isAnimate })}>
-      <div ref={ref} className="clef-disc__ref-container">
-        <div className="clef-disc__draw-container" style={{ transform: `rotate(${angle}deg)` }}>
-          <ErrorBoundary fallbackRender={gradientCircleFallbackRender}>
-            {(isRendering || colors === undefined) && <DefaultCircle angle={angle} />}
-            {colors !== undefined && <GradientCircle colors={colors} onRendered={handleRendered} />}
-          </ErrorBoundary>
-          <div className="clef-disc__note">
-            <Note />
+    return (
+      <div className={cn('clef-disc', { 'clef-disc_animate': isAnimate })}>
+        <div ref={ref} className="clef-disc__ref-container">
+          <div className="clef-disc__draw-container" style={{ transform: `rotate(${angle}deg)` }}>
+            <ErrorBoundary fallbackRender={gradientCircleFallbackRender}>
+              {(isRendering || colors === undefined) && <DefaultCircle angle={angle} />}
+              {colors !== undefined && <GradientCircle colors={colors} onRendered={handleRendered} />}
+            </ErrorBoundary>
+            <div className="clef-disc__note">
+              <Note />
+            </div>
           </div>
         </div>
+        {borderColor !== '' && <div className="clef-disc__border" style={{ borderColor }} />}
       </div>
-      {borderColor !== '' && <div className="clef-disc__border" style={{ borderColor }} />}
-    </div>
-  );
-});
+    );
+  }
+);
 
 export { Disc, Props };
